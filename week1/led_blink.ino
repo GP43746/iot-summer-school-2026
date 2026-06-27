2a4cd642cba524c2237303bf71f492d219021e66
Gauransh <gauranshpundir@gmail.com>
2026-06-27 16:11:00 +0530

    feat: integrate potentiometer to scale blink speed dynamically (v1.2)

--- week1/led_blink.ino
+++ week1/led_blink.ino
@@ -1,18 +1,24 @@
 int blinkCount = 0;
+const int potPin = A0; 
 
 void setup() {
   pinMode(9, OUTPUT);
   Serial.begin(9600);
-
 }
 
 void loop() {
-  digitalWrite(9, HIGH);
-  delay(500);
-  digitalWrite(9, LOW);
-  delay(500);
+  int potValue = analogRead(potPin); 
+  int blinkDelay = map(potValue, 0, 1023, 100, 2000); 
 
+  digitalWrite(9, HIGH);
+  delay(blinkDelay);
+  digitalWrite(9, LOW);
+  delay(blinkDelay);
+  
   blinkCount++;
   Serial.print("Blink count: ");
-  Serial.println(blinkcount);
+  Serial.print(blinkCount);
+  Serial.print(" | Delay: ");
+  Serial.concat(blinkDelay);
+  Serial.println("ms");
 }

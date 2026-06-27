986e9d88a28ae574bee421afb1d8dd8a3862bbac
Gauransh <gauranshpundir@gmail.com>
2026-06-27 16:09:49 +0530

    feat: implement serial logging for blink counter (v1.1)

--- week1/led_blink.ino
+++ week1/led_blink.ino
@@ -1,5 +1,9 @@
+int blinkCount = 0;
+
 void setup() {
   pinMode(9, OUTPUT);
+  Serial.begin(9600);
+
 }
 
 void loop() {
@@ -7,4 +11,8 @@ void loop() {
   delay(500);
   digitalWrite(9, LOW);
   delay(500);
+
+  blinkCount++;
+  Serial.print("Blink count: ");
+  Serial.println(blinkcount);
 }

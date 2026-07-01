18abb4deece00f47dfff3f7e6c8d8cd42eb5d466
Gauransh <gauranshpundir@gmail.com>
2026-07-01 16:04:27 +0530

    feat: integrate 5th button scale modifier toggle for Major/Minor shifts

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -4,12 +4,15 @@ const int KEY_DO = 2;
 const int KEY_RE = 3;
 const int KEY_MI = 4;
 const int KEY_FA = 5;
+const int SCALE_TOGGLE_BUTTON = 6;
 
-const int NOTE_DO = 262;
-const int NOTE_RE = 294;
-const int NOTE_MI = 330;
-const int NOTE_FA = 349;
+const int NOTE_DO_MAJ = 262; const int NOTE_DO_MIN = 261;
+const int NOTE_RE_MAJ = 294; const int NOTE_RE_MIN = 293;
+const int NOTE_MI_MAJ = 330; const int NOTE_MI_MIN = 311;
+const int NOTE_FA_MAJ = 349; const int NOTE_FA_MIN = 349;
 const int NOTE_SOL_CHORD = 392;
+
+bool isMinorScale = false;
 
 void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
@@ -17,9 +20,15 @@ void setup() {
   pinMode(KEY_RE, INPUT_PULLUP);
   pinMode(KEY_MI, INPUT_PULLUP);
   pinMode(KEY_FA, INPUT_PULLUP);
+  pinMode(SCALE_TOGGLE_BUTTON, INPUT_PULLUP);
 }
 
 void loop() {
+  if (digitalRead(SCALE_TOGGLE_BUTTON) == LOW) {
+    isMinorScale = !isMinorScale;
+    delay(300);
+  }
+
   bool btnDo = (digitalRead(KEY_DO) == LOW);
   bool btnRe = (digitalRead(KEY_RE) == LOW);
   bool btnMi = (digitalRead(KEY_MI) == LOW);
@@ -31,10 +40,15 @@ void loop() {
     tone(BUZZER_PIN, NOTE_SOL_CHORD);
   } 
   else if (activeKeysCount == 1) {
-    if (btnDo) tone(BUZZER_PIN, NOTE_DO);
-    else if (btnRe) tone(BUZZER_PIN, NOTE_RE);
-    else if (btnMi) tone(BUZZER_PIN, NOTE_MI);
-    else if (btnFa) tone(BUZZER_PIN, NOTE_FA);
+    if (btnDo) {
+      tone(BUZZER_PIN, isMinorScale ? NOTE_DO_MIN : NOTE_DO_MAJ);
+    } else if (btnRe) {
+      tone(BUZZER_PIN, isMinorScale ? NOTE_RE_MIN : NOTE_RE_MAJ);
+    } else if (btnMi) {
+      tone(BUZZER_PIN, isMinorScale ? NOTE_MI_MIN : NOTE_MI_MAJ);
+    } else if (btnFa) {
+      tone(BUZZER_PIN, isMinorScale ? NOTE_FA_MIN : NOTE_FA_MAJ);
+    }
   } 
   else {
     noTone(BUZZER_PIN);
